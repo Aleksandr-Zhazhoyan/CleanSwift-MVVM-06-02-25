73df5f54bb74536a68eeee3590abb7539789c8c9
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-03-12 19:25:08 +0400

    Initial Commit

---
+++ CleanSwift-MVVM-06-02-25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwift-MVVM-06-02-25
+//
+//  Created by Aleksandr Zhazhoyan on 12.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
